8870042354d740174e26f317e319a138fb0d79ba
Gabriel Buades <gbuades@soffid.com>
2019-05-04 22:59:08 +0200

    Fix javadoc errors

--- mdagenerator/src/main/java/com/soffid/mda/generator/ValueObjectGenerator.java
+++ mdagenerator/src/main/java/com/soffid/mda/generator/ValueObjectGenerator.java
@@ -130,7 +130,7 @@ public class ValueObjectGenerator {
 				+ "\t{" + endl
 				+ "\t}" + endl + endl
 				+ "\t/**" + endl
-				+ "\t *  @see java.lang.object#toString()" + endl
+				+ "\t *  @see java.lang.Object#toString()" + endl
 				+ "\t */" + endl
 				+ "\tpublic String toString()" + endl
 				+ "\t{" + endl
@@ -221,12 +221,12 @@ public class ValueObjectGenerator {
 				+ "\t/**" + endl
 				+ "\t * This method allows the deserialization of an instance of this enumeration type to return the actual instance" + endl
 				+ "\t * that will be the singleton for the JVM in which the current thread is running." + endl
-				+ "\t * <p/>" + endl
+				+ "\t * " + endl
 				+ "\t * Doing this will allow users to safely use the equality operator <code>==</code> for enumerations because" + endl
 				+ "\t * a regular deserialized object is always a newly constructed instance and will therefore never be" + endl
 				+ "\t * an existing reference; it is this <code>readResolve()</code> method which will intercept the deserialization" + endl
 				+ "\t * process in order to return the proper singleton reference." + endl
-				+ "\t * <p/>" + endl
+				+ "\t * " + endl
 				+ "\t * This method is documented here:" + endl
 				+ "\t * <a href=\"http://java.sun.com/j2se/1.3/docs/guide/serialization/spec/input.doc6.html\">Java" + endl
 				+ "\t * Object Serialization Specification</a>" + endl
